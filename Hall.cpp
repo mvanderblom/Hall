ee52f011b7ea4c21bb7236b1e9cad13478364568
Martijn van der Blom <m.vanderblom@gmail.com>
2017-02-03 22:16:26 +0100

    mem optimization

--- Hall.cpp
+++ Hall.cpp
@@ -1,24 +1,18 @@
-#include "Hall.h"
-#include "Arduino.h"
+#include <Hall.h>
+#include <Arduino.h>
 
 Hall::Hall(int pin, int zeroReading) {
 	this->_pin = pin;
 	this->_zeroReading = zeroReading;
 }
 
-void Hall::debug() {
-	this->_debug = true;
-}
-
 void Hall::init() {
 	pinMode(_pin, INPUT);
-	if(_debug){
-		Serial.print("Initialized Hall sensor on pin: ");
-		Serial.print(_pin);
-		Serial.print(", zeroReading: ");
-		Serial.print(_zeroReading);
-		Serial.println();
-	}
+//	Serial.print("Initialized Hall sensor on pin: ");
+//	Serial.print(_pin);
+//	Serial.print(", zeroReading: ");
+//	Serial.print(_zeroReading);
+//	Serial.println();
 }
 
 int Hall::readRaw() {
@@ -28,4 +22,3 @@ int Hall::readRaw() {
 int Hall::read() {
 	return readRaw() - _zeroReading;
 }
-
